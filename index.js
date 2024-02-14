deb9aa3b181467d68e520708a450ea84f50ce67c
asrf786 <asrf786@gmail.com>
2024-02-14 00:35:13 +0000

    Random number create and add else if loop

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Generate random number
+const magicNumber = Math.floor(Math.random() * 20 + 1);
+
+document.getElementById("guess-number").textContent = magicNumber;
+let message = document.getElementById("msg");
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess-input").value);
+  if (!guess) {
+    message.textContent = "No Value entered";
+  } else if (guess === magicNumber) {
+    message.textContent = "Correct Number";
+  } else if (guess > magicNumber) {
+    message.textContent = "Your Number is Too high !";
+  } else {
+    message.textContent = "Your Number is Too Low !";
+  }
+});
