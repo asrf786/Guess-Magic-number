b5f8d9654b353a66ed717b03bf8ed46572051a8b
asrf786 <asrf786@gmail.com>
2024-02-16 01:22:30 +0000

    add all event using DOM

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 "use strict";
 
 // Generate random number
-const magicNumber = Math.floor(Math.random() * 20 + 1);
+let magicNumber = Math.floor(Math.random() * 20 + 1);
 let trials = 20;
 let highscore = 0;
 
-document.getElementById("guess-number").textContent = magicNumber;
 let message = document.getElementById("msg");
-const inputValue = document.querySelector(".guess-input");
+let inputValue = document.querySelector(".guess-input");
 let trialValue = document.querySelector("#trials");
 let highscoreValue = document.querySelector("#high-score");
 
+document.querySelector(".new-game").classList.add("disabled");
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(inputValue.value);
+
   if (trials > 1) {
+    //If no number Enter
     if (!guess) {
       message.textContent = "No Value entered";
-    } else if (guess === magicNumber) {
+    }
+    //If number is Same number
+    else if (guess === magicNumber) {
       message.textContent = "Correct Number ! You WON !";
-      trialValue.textContent = trials;
+      message.style.backgroundColor = "#60b347";
+
+      document.getElementById("guess-number").textContent = magicNumber;
+      inputValue.style.backgroundColor = "#60b347";
+      trialValue.textContent = "Won";
+
       if (trials > highscore) {
         highscore = trials;
         highscoreValue.textContent = trials;
       }
+      document.querySelector(".check").classList.add("disabled");
+
+      document.querySelector(".new-game").classList.remove("disabled");
+      //IF number id greater than random number
     } else if (guess > magicNumber) {
       message.textContent = "Your Number is Too high !";
       trials--;
@@ -34,6 +48,29 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   } else {
     message.textContent = "You LOST the Game";
+    inputValue.style.backgroundColor = "red";
+    message.style.backgroundColor = "red";
+
+    document.getElementById("guess-number").textContent = magicNumber;
     trialValue.textContent = "0";
+    document.querySelector(".check").classList.add("disabled");
+
+    document.querySelector(".new-game").classList.remove("disabled");
   }
+});
+
+//Play again button Event
+
+document.querySelector(".new-game").addEventListener("click", function () {
+  document.getElementById("guess-number").textContent = "?";
+  document.querySelector(".new-game").classList.add("disabled");
+  document.querySelector(".check").classList.remove("disabled");
+  magicNumber = Math.floor(Math.random() * 20 + 1);
+  inputValue.style.backgroundColor = "#ffff";
+  message.style.backgroundColor = "";
+  trials = 20;
+  message.textContent = "Start Guessing for New Number ! PLZ";
+  inputValue.value = "";
+
+  trialValue.textContent = trials;
 });
